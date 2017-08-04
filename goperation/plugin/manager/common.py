f100653371fc91288da93eb3b8c253093d362776
lolizeppelin <lolizeppelin@gmail.com>
2017-08-04 10:39:18 +0800

    定义静态变量值

--- goperation/plugin/manager/common.py
+++ goperation/plugin/manager/common.py
@@ -9,19 +9,25 @@ RPC_SEND_RETRY = 1
 AGENT = 'agent'
 APPLICATION = 'application'
 SCHEDULER = 'scheduler'
-# status of agent
 
 
+# -----------status of agent--------------
 ACTIVE = 1
 UNACTIVE = 0
 INITIALIZING = -1
-SOFTBUSY = - 99
-HARDBUSY = -100
+SOFTBUSY = -9
+HARDBUSY = -10
 DELETED = -127
 # per delete status can not be recode into database
 # agent set status as PERDELETE when get a rpc cast of delete_agent_precommit
 PERDELETE = -128
+# -----------status of agent--------------
 
+
+# ------status of async request-----------
+FINISH = 1
+UNFINISH = 0
+# ------status of async request-----------
 
 # default time of agent status key in redis
 ONLINE_EXIST_TIME = 600
