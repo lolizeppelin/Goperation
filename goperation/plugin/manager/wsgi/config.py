38faebf3688eed70930189adbf65ee709afd6b28
lolizeppelin <lolizeppelin@gmail.com>
2017-08-14 10:04:17 +0800

    找绝对路径的方法移到外部

--- goperation/plugin/manager/wsgi/config.py
+++ goperation/plugin/manager/wsgi/config.py
@@ -1,4 +1,3 @@
-import os
 from simpleutil.config import cfg
 
 CONF = cfg.CONF
@@ -9,15 +8,3 @@ route_opts = [
                  default=[],
                  help='Manager extend route module string'),
 ]
-
-def find_paste_abs(conf):
-    # isure paste_deploy config
-    if not conf.paste_config:
-        raise TypeError('Paste config is None')
-    if not os.path.isabs(conf.paste_config):
-        paste_path = CONF.find_file(conf.paste_config)
-    else:
-        paste_path = conf.paste_config
-    if not paste_path:
-        raise TypeError('Paste config is None')
-    return paste_path
