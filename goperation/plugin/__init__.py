1a66e44e0d999d4dd2189740561b0dab600ab52c
lolizeppelin <lolizeppelin@gmail.com>
2017-08-18 11:47:12 +0800

    增加一个公共线程池

--- goperation/plugin/__init__.py
+++ goperation/plugin/__init__.py
@@ -1,3 +1,6 @@
-#  For wsgi
+from simpleutil.utils.threadgroup import ThreadGroup
+#  Routes or wsgi
 CORE_ROUTES = []
 EXTEND_ROUTES = []
+# work pool for plugin servies
+pool = ThreadGroup(thread_pool_size=100)
