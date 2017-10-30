cb8f26947c20b1672659f75f4224c8bc2b2933d6
lolizeppelin <lolizeppelin@gmail.com>
2017-10-30 14:19:44 +0800

    全局锁相关参数放入这里

--- goperation/manager/config.py
+++ goperation/manager/config.py
@@ -6,11 +6,14 @@ from simpleservice.rpc.driver.config import rabbit_opts
 from simpleservice.rpc.driver.config import amqp_opts
 
 from goperation.redis.config import redis_opts
-from goperation.filemanager.config import filemanager_opts
 
+goperation_opts = [
+    cfg.FloatOpt('glock_alloctime',
+                 default=1.5,
+                 help='Timeout for allocate glock')
+]
 
 CONF = cfg.CONF
-
 
 manager_group = cfg.OptGroup(name='manager', title='Manager base options')
 CONF.register_group(manager_group)
@@ -18,9 +21,8 @@ CONF.register_group(manager_group)
 manager_rabbit_group = cfg.OptGroup(name='rabbit', title='Manager RabbitMQ driver options')
 CONF.register_group(manager_rabbit_group)
 
-filemanager_group = cfg.OptGroup(name='filemanager', title='FileManager options')
-CONF.register_group(filemanager_group)
-
+# goperation opts for manager
+CONF.register_opts(goperation_opts, manager_group)
 # database for manager
 CONF.register_opts(database_opts, manager_group)
 # redis for manager
@@ -29,7 +31,5 @@ CONF.register_opts(redis_opts, manager_group)
 CONF.register_opts(rpc_base_opts, manager_rabbit_group)
 CONF.register_opts(amqp_opts, manager_rabbit_group)
 CONF.register_opts(rabbit_opts, manager_rabbit_group)
-# filemanager for manager
-CONF.register_opts(filemanager_opts, filemanager_group)
 # reset default value of rabbit_virtual_host
 CONF.set_default('rabbit_virtual_host', default='goperation', group=manager_rabbit_group)
