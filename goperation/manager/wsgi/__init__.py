f64312f144056972724f45c1e9bba87c32c6a99d
lolizeppelin <lolizeppelin@gmail.com>
2017-11-01 11:07:28 +0800

    路由加载位置更改

---
+++ goperation/manager/wsgi/__init__.py
@@ -0,0 +1,17 @@
+from goperation import CORE_ROUTES
+from goperation import EXTEND_ROUTES
+from goperation.manager.wsgi.agent import routers as agent_routes
+from goperation.manager.wsgi.file import routers as file_routes
+from goperation.manager.wsgi.port import routers as port_routes
+from goperation.manager.wsgi.endpoint import routers as endpoint_routes
+from goperation.manager.wsgi.entity import routers as entity_routes
+from goperation.manager.wsgi.cache import routers as cache_routes
+from goperation.manager.wsgi.asyncrequest import routers as asyncrequest_routes
+from goperation.manager.wsgi.agent.scheduler import routers as scheduler_routes
+from goperation.manager.wsgi.agent.application import routers as application_routes
+
+CORE_ROUTES.extend([asyncrequest_routes, agent_routes,
+                    port_routes, endpoint_routes, entity_routes,
+                    scheduler_routes, application_routes, cache_routes])
+
+EXTEND_ROUTES.extend([file_routes, ])
