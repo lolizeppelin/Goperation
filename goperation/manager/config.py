00d7e1eea7d62ee60bdae470759c447e6c73b2a8
lolizeppelin <lolizeppelin@gmail.com>
2017-11-02 12:31:18 +0800

    rabbit配置合并到manager中

--- goperation/manager/config.py
+++ goperation/manager/config.py
@@ -7,20 +7,36 @@ from simpleservice.rpc.driver.config import amqp_opts
 
 from goperation.redis.config import redis_opts
 
+CONF = cfg.CONF
+
+
 goperation_opts = [
+    cfg.StrOpt('rabbit',
+               default='Manager rabbitmq config group name',
+               help=''
+               ),
     cfg.FloatOpt('glock_alloctime',
                  default=1.5,
-                 help='Timeout for allocate glock')
+                 help='Timeout for allocate glock'),
+    cfg.HostnameOrIPOpt('gcenter',
+                        help='Hostname or IP address of gcenter wsgi service'
+                        ),
+    cfg.PortOpt('gcenter_port',
+                default=7999,
+                help='Http port of gcenter wsgi service'),
+    cfg.IntOpt('http_pconn_count',
+               min=5,
+               max=50,
+               default=30,
+               help='HTTP persistent connection number for gcenter'),
+    cfg.StrOpt('trusted',
+               default='goperation-trusted-user',
+               help='Trusted token, means a unlimit user'
+               ),
 ]
-
-CONF = cfg.CONF
 
 manager_group = cfg.OptGroup(name='manager', title='Manager base options')
 CONF.register_group(manager_group)
-
-manager_rabbit_group = cfg.OptGroup(name='rabbit', title='Manager RabbitMQ driver options')
-CONF.register_group(manager_rabbit_group)
-
 # goperation opts for manager
 CONF.register_opts(goperation_opts, manager_group)
 # database for manager
@@ -28,8 +44,8 @@ CONF.register_opts(database_opts, manager_group)
 # redis for manager
 CONF.register_opts(redis_opts, manager_group)
 # rabbit for manager
-CONF.register_opts(rpc_base_opts, manager_rabbit_group)
-CONF.register_opts(amqp_opts, manager_rabbit_group)
-CONF.register_opts(rabbit_opts, manager_rabbit_group)
+CONF.register_opts(rpc_base_opts, manager_group)
+CONF.register_opts(amqp_opts, manager_group)
+CONF.register_opts(rabbit_opts, manager_group)
 # reset default value of rabbit_virtual_host
-CONF.set_default('rabbit_virtual_host', default='goperation', group=manager_rabbit_group)
+CONF.set_default('rabbit_virtual_host', default='goperation', group=manager_group)
