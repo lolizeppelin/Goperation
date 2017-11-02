8fffcbdfd30ab6fc44236b42c440462bcaaf07d6
lolizeppelin <lolizeppelin@gmail.com>
2017-11-02 12:32:03 +0800

    增加一个gcenter的公共http连接池

--- goperation/manager/api.py
+++ goperation/manager/api.py
@@ -1,3 +1,6 @@
+from requests import Session
+from requests import adapters
+
 from simpleutil.config import cfg
 from simpleutil.utils import timeutils
 from simpleutil.log import log as logging
@@ -10,8 +13,8 @@ from simpleservice.rpc.config import rpc_client_opts
 
 from goperation import lock
 from goperation.redis import GRedisPool
+from goperation.api.client import ManagerClient
 from goperation.manager.config import manager_group
-from goperation.manager.config import manager_rabbit_group
 from goperation.manager.gdata import GlobalData
 
 
@@ -23,7 +26,51 @@ DbDriver = None
 GRedis = None
 SERVER_ID = None
 RPCClient = None
+HTTPClient = None
 GlobalDataClient = None
+
+
+class ManagerRpcClient(RPCClientBase):
+    """singleton Rpc client"""
+    def __init__(self):
+        CONF.register_opts(rpc_client_opts, manager_group)
+        super(ManagerRpcClient, self).__init__(CONF[manager_group.name])
+        self.rpcdriver.init_timeout_record(session=get_session(readonly=False))
+
+
+def rpcfinishtime(starttime=None):
+    rpc_conf = CONF[manager_group.name]
+    if not starttime:
+        starttime = int(timeutils.realnow())
+    offset_time = rpc_conf.rpc_send_timeout * (rpc_conf.rpc_send_retry + 1)
+    return starttime + offset_time + 4
+
+
+def init_server_id():
+    global SERVER_ID
+    if SERVER_ID is None:
+        with lock.get('sid'):
+            if SERVER_ID is None:
+                session = get_session()
+                # result = session.query(GkeyMap).filter(GkeyMap.host ==  CONF.host).with_for_update().one_or_none()
+                # if not result:
+                #     session.query(GkeyMap).filter(GkeyMap.host ==  None).update(dict(host=CONF.host),
+                #                                                                 update_args={'mysql_limit': 1})
+                #     session.commit()
+                # else:
+                #     session.commit()
+                #     return result.sid
+                with session.begin(subtransactions=True):
+                    query = model_query(session, GkeyMap, filter=GkeyMap.host == CONF.host)
+                    result = query.one_or_none()
+                    if not result:
+                        upquery = model_query(session, GkeyMap, filter=GkeyMap.host is None)
+                        upquery.update(dict(host=CONF.host),
+                                       update_args={'mysql_limit': 1})
+                        result = query.one()
+                    SERVER_ID = result.sid
+    else:
+        LOG.warning("Do not call init_server_id more then once")
 
 
 def init_mysql_session():
@@ -46,33 +93,6 @@ def get_session(readonly=False):
     return DbDriver.get_session(read=readonly,
                                 autocommit=True,
                                 expire_on_commit=False)
-
-
-def init_server_id():
-    global SERVER_ID
-    if SERVER_ID is None:
-        with lock.get('sid'):
-            if SERVER_ID is None:
-                session = get_session()
-                # result = session.query(GkeyMap).filter(GkeyMap.host ==  CONF.host).with_for_update().one_or_none()
-                # if not result:
-                #     session.query(GkeyMap).filter(GkeyMap.host ==  None).update(dict(host=CONF.host),
-                #                                                                 update_args={'mysql_limit': 1})
-                #     session.commit()
-                # else:
-                #     session.commit()
-                #     return result.sid
-                with session.begin(subtransactions=True):
-                    query = model_query(session, GkeyMap, filter=GkeyMap.host == CONF.host)
-                    result = query.one_or_none()
-                    if not result:
-                        upquery = model_query(session, GkeyMap, filter=GkeyMap.host == None)
-                        upquery.update(dict(host=CONF.host),
-                                       update_args={'mysql_limit': 1})
-                        result = query.one()
-                    SERVER_ID = result.sid
-    else:
-        LOG.warning("Do not call init_server_id more then once")
 
 
 def init_redis():
@@ -127,35 +147,42 @@ def get_client():
     return RPCClient
 
 
-def rpcfinishtime(starttime=None):
-    rpc_conf = CONF[manager_rabbit_group.name]
-    if not starttime:
-        starttime = int(timeutils.realnow())
-    offset_time = rpc_conf.rpc_send_timeout * (rpc_conf.rpc_send_retry + 1)
-    return starttime + offset_time + 4
-
-
-def init_global_data_client():
+def init_global():
     global GlobalDataClient
     if GlobalDataClient is None:
-        with lock.get('sync'):
+        with lock.get('global'):
             if GlobalDataClient is None:
                 LOG.info("Try init glock client for manager")
                 GlobalDataClient = GlobalData(client=get_redis(),
                                               session=get_session)
     else:
-        LOG.warning("Do not call init_sync_client more then once")
+        LOG.warning("Do not call init_global more then once")
 
 
 def get_global():
     if GlobalDataClient is None:
-        init_global_data_client()
+        init_global()
     return GlobalDataClient
 
 
-class ManagerRpcClient(RPCClientBase):
-    """singleton Rpc client"""
-    def __init__(self):
-        CONF.register_opts(rpc_client_opts, manager_rabbit_group)
-        super(ManagerRpcClient, self).__init__(CONF[manager_rabbit_group.name])
-        self.rpcdriver.init_timeout_record(session=get_session(readonly=False))
+def init_http_client():
+    global HTTPClient
+    if HTTPClient is None:
+        with lock.get('http'):
+            if HTTPClient is None:
+                LOG.info("Try init http client for manager")
+                conf = CONF[manager_group.name]
+                _Session = Session()
+                _Session.mount('http://', adapters.HTTPAdapter(pool_connections=1,
+                                                               pool_maxsize=conf.http_pconn_count))
+                HTTPClient = ManagerClient(url=conf.gcenter,
+                                           port=conf.gcenter_port,
+                                           token=conf.trusted, session=_Session)
+    else:
+        LOG.warning("Do not call init_http_client more then once")
+
+
+def get_http():
+    if HTTPClient is None:
+        init_http_client()
+    return HTTPClient
