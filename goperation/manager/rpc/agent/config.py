2732b65db5a843b02549c8fc2deed4e58536c565
lolizeppelin <lolizeppelin@gmail.com>
2017-11-02 12:33:30 +0800

    gcenter定位相关配置从agent配置移到manager的配置中

--- goperation/manager/rpc/agent/config.py
+++ goperation/manager/rpc/agent/config.py
@@ -9,17 +9,6 @@ CONF.register_group(agent_group)
 
 
 rpc_agent_opts = [
-    cfg.HostnameOrIPOpt('gcenter',
-                        help='Hostname or IP address of gcenter wsgi service'
-                        ),
-    cfg.PortOpt('gcenter_port',
-                default=7999,
-                help='Http port of gcenter wsgi service'),
-    cfg.IntOpt('http_pconn_count',
-               min=5,
-               max=50,
-               default=30,
-               help='HTTP persistent connection number for gcenter'),
     cfg.MultiOpt('ports_range',
                  item_type=cfg.types.PortRange(),
                  help='Rpc agent can alloc port from this range'),
@@ -32,8 +21,8 @@ rpc_agent_opts = [
                 default=False,
                 help='Rpc agent online report with system performance'),
     cfg.StrOpt('taskflow_storage',
-                default='sqlite:///:memory:',
-                help='simpleflow storage connection url'),
+               default='sqlite:///:memory:',
+               help='simpleflow storage connection url'),
 ]
 
 rpc_endpoint_opts = [
