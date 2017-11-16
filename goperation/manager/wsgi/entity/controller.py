1211235d93422e657dc72d0a6dab9c3b1ddf1493
lolizeppelin <lolizeppelin@gmail.com>
2017-11-16 12:37:38 +0800

    delete 将发出rpc call操作

--- goperation/manager/wsgi/entity/controller.py
+++ goperation/manager/wsgi/entity/controller.py
@@ -21,9 +21,12 @@ from simpleservice.rpc.exceptions import NoSuchMethod
 from goperation.manager import common as manager_common
 from goperation.manager import utils
 from goperation.manager import resultutils
+from goperation.manager import targetutils
 
 from goperation.manager.api import get_global
 from goperation.manager.api import get_session
+from goperation.manager.api import get_client
+from goperation.manager.api import rpcfinishtime
 from goperation.manager.models import AgentEntity
 from goperation.manager.models import AllocatedPort
 
@@ -47,6 +50,7 @@ FAULT_MAP = {InvalidArgument: webob.exc.HTTPClientError,
              NoResultFound: webob.exc.HTTPNotFound,
              MultipleResultsFound: webob.exc.HTTPInternalServerError,
              }
+
 
 @singleton.singleton
 class EntityReuest(BaseContorller):
@@ -79,7 +83,7 @@ class EntityReuest(BaseContorller):
             ports = argutils.map_with(ports, validators['type:port'])
             used_ports = model_count_with_key(session, AllocatedPort.port,
                                               filter=and_(AllocatedPort.port.in_(ports),
-                                              AllocatedPort.agent_id == agent_id))
+                                                          AllocatedPort.agent_id == agent_id))
             if used_ports:
                 raise InvalidArgument('Ports has been used %d' % used_ports)
         desc = body.get('desc')
@@ -123,13 +127,13 @@ class EntityReuest(BaseContorller):
 
     def delete(self, req, endpoint, entity, body=None):
         body = body or {}
-        force = body.get('force', False)
+        force = body.pop('force', False)
         endpoint = utils.validate_endpoint(endpoint)
         entitys = argutils.map_to_int(entity)
         session = get_session()
         glock = get_global().lock('entitys')
         elock = get_global().lock('endpoint')
-        agent_id = None
+        agent = None
         with glock(entitys):
             with elock(endpoint):
                 with session.begin():
@@ -138,16 +142,24 @@ class EntityReuest(BaseContorller):
                                                     AgentEntity.entity.in_(entitys)))
                     if not force:
                         for entity in query:
-                            if agent_id is None:
-                                agent_id = entity.agent_id
+                            if agent is None:
+                                agent = entity.agent
                                 continue
-                            if entity.agent_id != agent_id:
+                            if entity.agent_id != agent.agent_id:
                                 raise InvalidArgument('Delete entity fail, entity not in same agent')
                     delete_count = query.delete()
                     need_to_delete = len(entitys)
                     if delete_count != len(entitys):
                         LOG.warning('Delete %d entitys, but expect count is %d' % (delete_count, need_to_delete))
                     if not force:
-                        pass
-
+                        rpc = get_client()
+                        target = targetutils.target_agent(agent)
+                        target.namespace = endpoint
+                        body.setdefault('entitys', entitys)
+                        delete_ret = rpc.call(target, ctxt={'finishtime': rpcfinishtime()},
+                                               msg={'method': 'delete_entitys', 'args': body})
+                        if not delete_ret:
+                            raise RpcResultError('delete entitys result is None')
+                        if delete_ret.get('resultcode') != manager_common.RESULT_SUCCESS:
+                            return resultutils.results(result=delete_ret.get('result'))
         return resultutils.results(result='delete endpoints success')
