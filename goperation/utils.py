f8fbbc59e3efb26fe07d7358af39c7f04c64ad66
lolizeppelin <lolizeppelin@gmail.com>
2017-11-20 14:07:57 +0800

    适配systemutils修改

--- goperation/utils.py
+++ goperation/utils.py
@@ -9,16 +9,6 @@ from simpleutil.log import log as logging
 
 from simpleservice.base import SignalHandler
 
-INTERVAL = 0.01
-
-
-class ExitBySIG(Exception):
-    """"""
-
-
-class UnExceptExit(Exception):
-    """"""
-
 
 def safe_func_wrapper(f, logger=None):
     try:
@@ -44,11 +34,8 @@ def nirvana(delay=1):
     pass
 
 
-
 if systemutils.LINUX:
     from simpleutil.utils.systemutils.posix import linux
-    import signal
-    import errno
 
     def safe_fork(user=None, group=None):
         # Disable gc to avoid bug where gc -> file_dealloc ->
@@ -63,10 +50,7 @@ if systemutils.LINUX:
                 gc.enable()
             raise
         if pid == 0:
-            if group:
-                linux.setgid(group)
-            if user:
-                linux.setuid(user)
+            linux.drop_privileges(group, user)
             os.umask(002)
             # set close exec for loggin
             logging.set_filehandler_close_exec()
@@ -98,63 +82,9 @@ if systemutils.LINUX:
                 gc.enable()
         return pid
 
-    def wait(pid, timeout=None):
-        used_time = 0.0
-        timeout = float(timeout) if timeout else None
-        while True:
-            try:
-                # same as eventlet.green.os.wait
-                _pid, status = os.waitpid(pid, os.WNOHANG)
-                if not _pid:
-                    if timeout and used_time > timeout:
-                        os.kill(pid, signal.SIGTERM)
-                        _pid, status = os.waitpid(pid, os.WNOHANG)
-                        if not _pid:
-                            os.kill(pid, signal.SIGKILL)
-                        os.waitpid(pid, 0)
-                        raise ExitBySIG('sub process terminated or killed')
-                    eventlet.sleep(INTERVAL)
-                    used_time += INTERVAL
-                    continue
-                else:
-                    if not os.WIFSIGNALED(status):
-                        code = os.WEXITSTATUS(status)
-                        if code != 0:
-                            raise UnExceptExit('sup process exit code %d' % code)
-                        break
-                    else:
-                        raise ExitBySIG('sub process exit with by signal, maybe timeout')
-            except OSError as exc:
-                if exc.errno not in (errno.EINTR, errno.ECHILD):
-                    raise OSError('waitpid get errno %d' % exc.errno)
-                continue
+    wait = linux.wait
 else:
-
     def safe_fork(*args):
         raise NotImplementedError
 
-    def wait(sub, timeout=None):
-        used_time = 0.0
-        timeout = float(timeout) if timeout else None
-        while True:
-            try:
-                # same as eventlet.green.os.wait
-                if sub.poll() is None:
-                    if timeout and used_time > timeout:
-                        sub.terminate()
-                        if sub.poll() is None:
-                            sub.kill()
-                        sub.wait()
-                        raise ExitBySIG('sub process exit with by signal, maybe timeout')
-                    eventlet.sleep(INTERVAL)
-                    used_time += INTERVAL
-                    continue
-                else:
-                    code = sub.wait()
-                    if code != 0:
-                        raise UnExceptExit('sup process exit code %d' % code)
-                    break
-            except OSError as exc:
-                if exc.errno not in (errno.EINTR, errno.ECHILD):
-                    raise OSError('waitpid get errorno %d' % exc.errno)
-                continue
+    wait = systemutils.subwait
