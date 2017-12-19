610bb02139a77dca35b03e7c589eaeb512cbf8c9
lolizeppelin <lolizeppelin@gmail.com>
2017-12-19 14:28:35 +0800

    增加一个带时间限制锁接口

--- goperation/__init__.py
+++ goperation/__init__.py
@@ -7,13 +7,29 @@ try:
 except ImportError:
     raise
 
+import contextlib
 from simpleutil.utils import lockutils
 from simpleutil.utils.threadgroup import ThreadGroup
 
-lock = lockutils.Semaphores()
 # public thread pool
 threadpool = ThreadGroup(thread_pool_size=100)
 
 #  Routes or wsgi
 CORE_ROUTES = []
 EXTEND_ROUTES = []
+
+# public locks
+lock = lockutils.Semaphores()
+
+@contextlib.contextmanager
+def tlock(target, timeout):
+    timeout = float(timeout)
+    _lock = lock.get(target)
+    if _lock.acquire(blocking=True, timeout=max(0.1, timeout)):
+        try:
+            yield
+        finally:
+            _lock.release()
+    else:
+        raise KeyError('alloc lock %s fail' % target)
+
