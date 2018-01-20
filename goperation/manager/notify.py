1bfd17a6fa4a7a3dcc3ae98bccff06ee16266bdb
lolizeppelin <lolizeppelin@gmail.com>
2018-01-20 12:44:51 +0800

    支持普通http请求,优化代码结构

--- goperation/manager/notify.py
+++ goperation/manager/notify.py
@@ -1,11 +1,15 @@
 import six
+import copy
 import abc
+import requests
 import inspect
 
 from simpleutil.log import log as logging
 from simpleutil.utils import jsonutils
 from simpleservice.rpc.target import Target
 
+import goperation
+from goperation.utils import safe_func_wrapper
 from goperation.manager.api import get_http
 from goperation.manager.api import get_client
 
@@ -41,6 +45,18 @@ NOTIFYSCHEMA = {
              'headers': {'oneOf': [{'type': 'null'}, {'type': 'object'}]},
              'params': {'oneOf': [{'type': 'null'}, {'type': 'object'}]},
              'timeout': {'oneOf': [{'type': 'null'}, {'type': 'integer', 'minimum': 3, 'maxmum': 30}]}}
+         },
+        {'type': 'object',
+         'required': ['method', 'url'],
+         'properties': {
+             'method': {'type': 'string',
+                        'enum': ['GET', 'DELETE', 'POST', 'PUT', 'HEAD', 'PATCH', 'OPTIONS']},
+             'url': {'type': 'string'},
+             'data': {'oneOf': [{'type': 'null'}, {'type': 'object'}]},
+             'json': {'oneOf': [{'type': 'null'}, {'type': 'object'}]},
+             'headers': {'oneOf': [{'type': 'null'}, {'type': 'object'}]},
+             'params': {'oneOf': [{'type': 'null'}, {'type': 'object'}]},
+             'timeout': {'oneOf': [{'type': 'null'}, {'type': 'integer', 'minimum': 3, 'maxmum': 30}]}}
          }
     ]
 }
@@ -48,8 +64,13 @@ NOTIFYSCHEMA = {
 
 @six.add_metaclass(abc.ABCMeta)
 class NotifyInterface(object):
+
     def __init__(self, notify):
         self.notify = notify
+
+    @abc.abstractmethod
+    def _do(self, keyword, replace=None):
+        """impl do"""
 
     @abc.abstractmethod
     def default(self, *args, **kwargs):
@@ -60,65 +81,76 @@ class NotifyInterface(object):
 
 
 class EmptyNotify(NotifyInterface):
+
     def default(self):
         """do nothing"""
 
 
 @six.add_metaclass(abc.ABCMeta)
 class GeneralNotify(object):
-    @abc.abstractmethod
+
     def success(self):
-        """when success"""
+        key = inspect.stack()[0][3]
+        self._do(key)
 
-    @abc.abstractmethod
     def fail(self):
-        """when fail"""
+        keyword = inspect.stack()[0][3]
+        self._do(keyword)
 
 
-class RpcNotify(NotifyInterface, GeneralNotify):
+class GopRpcNotify(NotifyInterface, GeneralNotify):
+
     def default(self):
         raise NotImplementedError
 
-    def success(self):
-        key = inspect.stack()[0][3]
-        if key not in self.notify:
+    def _do(self, keyword, replace=None):
+        if keyword not in self.notify:
             return
-        data = self.notify[key]
-        target = Target(**data.pop('target'))
-        func = getattr(get_client(), data.pop('method'))
-        func(target, **data)
 
-    def fail(self):
-        key = inspect.stack()[0][3]
-        if key not in self.notify:
-            return
-        data = self.notify[key]
-        target = Target(**data.pop('target'))
-        func = getattr(get_client(), data.pop('method'))
-        func(target, **data)
+        def wapper():
+            data = self.notify[keyword]
+            target = Target(**data.pop('target'))
+            func = getattr(get_client(), data.pop('method'))
+            func(target, **data)
+
+        goperation.threadpool.add_thread(safe_func_wrapper, wapper, LOG)
 
 
-class HttpNotify(NotifyInterface, GeneralNotify):
+class GopHttpNotify(NotifyInterface, GeneralNotify):
+
     def default(self):
         raise NotImplementedError
 
-    def success(self):
-        LOG.debug('Notify success called')
-        key = inspect.stack()[0][3]
-        if key not in self.notify:
+    def _do(self, keyword, replace=None):
+        LOG.debug('Notify %s called' % keyword)
+        if keyword not in self.notify:
             return
-        data = self.notify[key]
-        func = getattr(get_http(), 'do_request')
-        func(**data)
 
-    def fail(self):
-        LOG.debug('Notify fail called')
-        key = inspect.stack()[0][3]
-        if key not in self.notify:
+        def wapper():
+            data = self.notify[keyword]
+            func = getattr(get_http(), 'do_request')
+            func(**data)
+
+        goperation.threadpool.add_thread(safe_func_wrapper, wapper, LOG)
+
+
+class HttpNotify(NotifyInterface):
+    def default(self):
+        raise NotImplementedError
+
+    def _do(self, keyword, replace=None):
+        LOG.debug('Notify %s called' % keyword)
+        if keyword not in self.notify:
             return
-        data = self.notify[key]
-        func = getattr(get_http(), 'do_request')
-        func(**data)
+
+        def wapper():
+            data = self.notify[keyword]
+            if replace:
+                data = copy.copy(data)
+                data.update(replace)
+            requests.request(**data)
+
+        goperation.threadpool.add_thread(safe_func_wrapper, wapper, LOG)
 
 
 def notify_prepare(notify):
@@ -128,12 +160,14 @@ def notify_prepare(notify):
     for data in six.itervalues(notify):
         jsonutils.schema_validate(data, NOTIFYSCHEMA)
         if 'target' in data:
-            notifys.add(RpcNotify)
+            notifys.add(GopRpcNotify)
         if 'action' in data:
+            notifys.add(GopHttpNotify)
+        if 'url' in data:
             notifys.add(HttpNotify)
     if len(notifys) != 1:
         notifys.clear()
-        raise ValueError('Notify type error!')
+        raise ValueError('Notify type error, more then one')
     cls = notifys.pop()
     for attrib in notify:
         if not hasattr(cls, attrib):
