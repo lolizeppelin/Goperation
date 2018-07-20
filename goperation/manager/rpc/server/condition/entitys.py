061b382094def26ce3eae95f146b0a7592ab751b
lolizeppelin <lolizeppelin@gmail.com>
2018-07-21 00:23:05 +0800

    小bug修复

--- goperation/manager/rpc/server/condition/entitys.py
+++ goperation/manager/rpc/server/condition/entitys.py
@@ -50,7 +50,7 @@ class Condition(condition.BaseCondition):
         joins = joins.joinedload(AgentRespone.details, innerjoin=False)
         query = query.options(joins)
         asyncrequest = query.one()
-        results = resultutils.async_request(asyncrequest, agents=True, details=True).get('data')[0]
+        results = resultutils.async_request(asyncrequest, agents=True, details=True)
         respones = results.get('respones')
 
         _count = 0
