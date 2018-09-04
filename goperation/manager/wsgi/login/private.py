3429fbadc5b4ecff92fd015cc262b3acd0b884d6
lolizeppelin <lolizeppelin@gmail.com>
2018-09-04 12:14:44 +0800

    小bug修复

--- goperation/manager/wsgi/login/private.py
+++ goperation/manager/wsgi/login/private.py
@@ -14,15 +14,7 @@ class Routers(router.RoutersBase):
 
     def append_routers(self, mapper, routers=None):
         controller_intance = controller_return_response(controller.LoginReuest(), controller.FAULT_MAP)
-        self._add_resource(mapper, controller_intance, path='/goperation/login/{username}',
-                           post_action='login')
         self._add_resource(mapper, controller_intance, path='/goperation/login/{username}/{token}',
                            delete_action='loginout')
         self._add_resource(mapper, controller_intance, path='/goperation/login/{username}/{token}',
                            post_action='expire')
-
-        # mapper.collection(collection_name=self.collection_name,
-        #                   resource_name=self.resource_name,
-        #                   controller=controller_intance,
-        #                   collection_actions=COLLECTION_ACTIONS,
-        #                   member_actions=MEMBER_ACTIONS)
